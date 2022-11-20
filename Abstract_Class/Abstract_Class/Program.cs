feca320448f423238828b104566395fdc005ca18
kalyani183 <114869398+kalyani183@users.noreply.github.com>
2022-11-20 19:24:55 +0530

    Program

---
+++ Abstract_Class/Abstract_Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+                   public abstract class Shape
+          {
+           public abstract void draw();
+          }
+          public class Rectangle:Shape
+          {
+           public override void draw()
+           {
+            Console.WriteLine("drawing Rectangle...");
+           }
+          }
+    
+        public class Circle :Shape
+        {
+         public override void draw()
+         {
+          Console.WriteLine("drawing Circle...");
+         }
+        }
+        public class TestAbstract
+        {
+          public static void Main()
+          {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadLine();
+          }
+
+        }
+    }
+}
