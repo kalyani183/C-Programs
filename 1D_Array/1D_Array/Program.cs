a5547d40c14e7f814fa443f7d93453443611be4f
kalyani183 <114869398+kalyani183@users.noreply.github.com>
2022-11-20 19:24:06 +0530

    Program

---
+++ 1D_Array/1D_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Books = new string[5];
+            Books[0] = "C#";
+            Books[1] = "Java";
+            Books[2] = "VB.NET";
+            Books[3] = "React";
+            Books[4] = "Python";
+
+            Console.WriteLine("All the element of Books array is:\n\n");
+
+            int i = 0;
+            Console.Write("\t1\t2\t3\t4\t5\n\n\t");
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("{0}\t", Books[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
