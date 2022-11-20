1ca1a7543a98cc8ded3e9d78d53f0071055bd313
kalyani183 <114869398+kalyani183@users.noreply.github.com>
2022-11-20 19:26:00 +0530

    Program

---
+++ Argument/Argument/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Argument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Length of argument" + args.Length);
+            Console.WriteLine("Arguments:");
+            foreach (Object obj in args)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
